6e6ad3c141aeac458d03467d7b112123c4d915f9
Leo <leo.lahti@iki.fi>
2020-12-29 22:56:49 +0200

    Updated documentation

--- R/LahtiWAData.R
+++ R/LahtiWAData.R
@@ -1,4 +1,4 @@
-#' Obtain the Lahti Western Adtult microbiome data
+#' Obtain the Lahti Western Adult microbiome data
 #'
 #' Obtain the microbiome data from Lahti et al. (2014).
 #'
@@ -6,6 +6,11 @@
 #' The LahtiWA dataset contains high-throughput genus-level microbiota profiling
 #' with HITChip for 1006 western adults with no reported health complications,
 #' reported in Lahti et al. (2014).
+#'
+#' This data set is based on the Human Intestinal Tract (HIT)Chip
+#' phylogenetic 16S microarray (Rajilić‐Stojanović _et al._ 2009.
+#' This profiling technology differs from the more widely used 16S rRNA
+#' amplicon sequencing.
 #'
 #' Column metadata includes the age, sex, nationality, DNA extraction method,
 #' project identifier, precomputed diversity measurement, BMI group,
@@ -18,12 +23,19 @@
 #'
 #' @return a \linkS4class{TreeSummarizedExperiment}
 #'
-#' @author Felix G.M. Ernst
+#' @author Felix G.M. Ernst and Leo Lahti
 #'
 #' @references
 #' Lahti L et al. (2014): Tipping elements in the human intestinal ecosystem.
 #' \emph{Nature Communications} 5:4344 (2014)
 #' \url{https://doi.org/10.1038/ncomms5344}
+#'
+#' Rajilić-Stojanović M, Heilig HG, Molenaar D, Kajander K, Surakka A, Smidt H,
+#' de Vos WM (2009). Development and application of the human intestinal tract chip, a
+#' phylogenetic microarray: analysis of universally conserved phylotypes in the
+#' abundant microbiota of young and elderly adults.
+#' \emph{Environ Microbiol.} 11(7):1736-51
+#' \url{https://doi.org/10.1111/j.1462-2920.2009.01900.x}
 #'
 #' @name LahtiWAData
 #' @export
@@ -38,3 +50,4 @@ LahtiWAData <- function() {
                        has.coldata = TRUE)
     tse
 }
+
