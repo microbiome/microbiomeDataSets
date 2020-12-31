ddf2c3aac06ebc4bf32d66ea70e7d20abfc683a5
FelixErnst <felix.gm.ernst@outlook.com>
2020-12-31 16:10:07 +0100

    metadata bugfix

--- inst/scripts/make-lahti-ml-metadata.R
+++ inst/scripts/make-lahti-ml-metadata.R
@@ -5,7 +5,14 @@ library(S4Vectors)
 df_Base <- DataFrame(
     BiocVersion = "3.13",
     SourceVersion = NA,
-    Coordinate_1_based = TRUE
+    Coordinate_1_based = TRUE,
+    Species = "Homo sapiens",
+    TaxonomyId = "9606",
+    SourceVersion = Sys.time(),
+    Genome = NA,
+    SourceUrl = "http://dx.doi.org/10.7717/peerj.32",
+    DataProvider = NA,
+    Maintainer = "Leo Lahti <leo.lahti@iki.fi>"
 )
 
 df <- rbind(
@@ -13,49 +20,36 @@ df <- rbind(
           DataFrame(Title = "Lahti ML microbiome counts",
                     Description = paste0("Count matrix for the Lahti microbiome dataset"),
                     SourceType = "CSV",
-                    SourceUrl = "http://dx.doi.org/10.7717/peerj.32",
-                    DataProvider = NA,
-                    Maintainer = "Leo Lahti <leo.lahti@iki.fi>",
                     RDataClass = "matrix",
                     DispatchClass = "Rds",
                     RDataPath = "microbiomeDataSets/lahti-ml/microbiome_counts.rds",
-                    Tags = NA),
+                    Tags = NA)),
+    cbind(df_Base,
           DataFrame(Title = "Lahti ML microbiome row data",
                     Description = paste0("Row data for the Lahti microbiome dataset"),
                     SourceType = "CSV",
-                    SourceUrl = "http://dx.doi.org/10.7717/peerj.32",
-                    DataProvider = NA,
-                    Maintainer = "Leo Lahti <leo.lahti@iki.fi>",
                     RDataClass = "DFrame",
                     DispatchClass = "Rds",
                     RDataPath = "microbiomeDataSets/lahti-ml/microbiome_rowdata.rds",
-                    Tags = NA),
+                    Tags = NA)),
+    cbind(df_Base,
           DataFrame(Title = "Lahti ML lipids counts",
                     Description = paste0("Count matrix for the Lahti lipids dataset"),
                     SourceType = "CSV",
-                    SourceUrl = "http://dx.doi.org/10.7717/peerj.32",
-                    DataProvider = NA,
-                    Maintainer = "Leo Lahti <leo.lahti@iki.fi>",
-                    RDataClass = "matrix",
+                   RDataClass = "matrix",
                     DispatchClass = "Rds",
                     RDataPath = "microbiomeDataSets/lahti-ml/lipids_counts.rds",
-                    Tags = NA),
+                    Tags = NA)),
+    cbind(df_Base,
           DataFrame(Title = "Lahti ML sample data",
                     Description = paste0("Sample data for the Lahti dataset"),
                     SourceType = "CSV",
-                    SourceUrl = "http://dx.doi.org/10.7717/peerj.32",
-                    DataProvider = NA,
-                    Maintainer = "Leo Lahti <leo.lahti@iki.fi>",
                     RDataClass = "DFrame",
                     DispatchClass = "Rds",
                     RDataPath = "microbiomeDataSets/lahti-ml/coldata.rds",
                     Tags = NA))
 )
 
-df$Species <- "Homo sapiens"
-df$TaxonomyId <- "9606"
-df$SourceVersion <- Sys.time()
-df$Genome <- NA
 df$Tags <- paste(df$Tags[!is.na(df$Tags)],"Microbiome",collapse = ":",sep="")
 
 write.csv(df, file = "inst/extdata/metadata-lahti-ml.csv", row.names = FALSE)
--- inst/scripts/make-lahti-wa-metadata.R
+++ inst/scripts/make-lahti-wa-metadata.R
@@ -5,7 +5,14 @@ library(S4Vectors)
 df_Base <- DataFrame(
     BiocVersion = "3.13",
     SourceVersion = NA,
-    Coordinate_1_based = TRUE
+    Coordinate_1_based = TRUE,
+    Species = "Homo sapiens",
+    TaxonomyId = "9606",
+    SourceVersion = Sys.time(),
+    Genome = NA,
+    SourceUrl = "https://doi.org/10.1038/ncomms5344",
+    DataProvider = NA,
+    Maintainer = "Leo Lahti <leo.lahti@iki.fi>"
 )
 
 df <- rbind(
@@ -13,39 +20,28 @@ df <- rbind(
           DataFrame(Title = "Lahti Western Adult microbiome counts",
                     Description = paste0("Count matrix for the Lahti Western Adult microbiome dataset"),
                     SourceType = "CSV",
-                    SourceUrl = "https://doi.org/10.1038/ncomms5344",
-                    DataProvider = NA,
-                    Maintainer = "Leo Lahti <leo.lahti@iki.fi>",
                     RDataClass = "matrix",
                     DispatchClass = "Rds",
                     RDataPath = "microbiomeDataSets/lahti-wa/counts.rds",
-                    Tags = NA),
+                    Tags = NA)),
+    cbind(df_Base,
           DataFrame(Title = "Lahti Western Adult microbiome row data",
                     Description = paste0("Row data for the Lahti Western Adult microbiome dataset"),
                     SourceType = "CSV",
-                    SourceUrl = "https://doi.org/10.1038/ncomms5344",
-                    DataProvider = NA,
-                    Maintainer = "Leo Lahti <leo.lahti@iki.fi>",
                     RDataClass = "DFrame",
                     DispatchClass = "Rds",
                     RDataPath = "microbiomeDataSets/lahti-wa/rowdata.rds",
-                    Tags = NA),
+                    Tags = NA)),
+    cbind(df_Base,
           DataFrame(Title = "Lahti Western Adult sample data",
                     Description = paste0("Sample data for the Lahti Western Adult dataset"),
                     SourceType = "CSV",
-                    SourceUrl = "https://doi.org/10.1038/ncomms5344",
-                    DataProvider = NA,
-                    Maintainer = "Leo Lahti <leo.lahti@iki.fi>",
                     RDataClass = "DFrame",
                     DispatchClass = "Rds",
                     RDataPath = "microbiomeDataSets/lahti-wa/coldata.rds",
                     Tags = NA))
 )
 
-df$Species <- "Homo sapiens"
-df$TaxonomyId <- "9606"
-df$SourceVersion <- Sys.time()
-df$Genome <- NA
 df$Tags <- paste(df$Tags[!is.na(df$Tags)],"Microbiome",collapse = ":",sep="")
 
 write.csv(df, file = "inst/extdata/metadata-lahti-wa.csv", row.names = FALSE)
