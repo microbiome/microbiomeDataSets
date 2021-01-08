6906185bb3d2fb5c86ae7f1ed82ede8dee88b7e0
Sudarshan <sudarshanshetty9@gmail.com>
2021-01-08 09:06:35 +0100

    iPOP first comit

---
+++ R/SailaniIPOPData.R
@@ -0,0 +1,95 @@
+#' Obtain the Sailani deep longitudinal multiomics data
+#'
+#' Obtain the deep longitudinal multiomics data from Sailani et al. (2020).
+#'
+#' @details
+#' The SailaniIPOP dataset contains longitudinal multiomics data from profiling 
+#' of 105 generally healthy individuals. This dataset consists of 16S rRNA gene 
+#' based profiling of gut and nasal microbiota. Host transcriptome, proteome and 
+#' metabolome from plasma, cytokine profile, and associated clinical data. The
+#' samples were collected over a period of 4 years. The data set can be used 
+#' to investigate longitudinal dynamics and interaction between microbiota and 
+#' host associated factors.  
+#'
+#' This microbiota data as currently available as relative abundances. Hierarchical
+#' taxonomic information is unavailable for microbiota data.
+#'
+#' Column metadata includes the time points, sex, subject identifier, sample
+#' identifier and treatment group.
+#'
+#'
+#' All data are downloaded from ExperimentHub and cached for local re-use.
+#'
+#' @return for  \code{SailaniIPOPData} a \linkS4class{MultiAssayExperiment} object
+#'   consisting of seven \linkS4class{SummarizedExperiment} objects.
+#'
+#' @author Sudarshan Shetty and Felix G.M. Ernst
+#'
+#' @references
+#' Sailani, M.R., Metwally, A.A., Zhou, W. et al. (2020) Deep longitudinal multiomics 
+#' profiling reveals two biological seasonal patterns in California.
+#' \emph{Nat Commun.} 11, 4933 \url{https://doi.org/10.1038/s41467-020-18758-1}
+#'
+#' Zhou, W., Sailani, M.R., Contrepois, K. et al. (2019) Longitudinal multi-omics of 
+#' host-microbe dynamics in prediabetes.
+#' \emph{Nature} 569, 663-671 \url{https://doi.org/10.1038/s41586-019-1236-x}
+#'
+#' @name SailaniIPOPData
+#' @export
+#'
+#' @examples
+#' mae <- SailaniIPOPData()
+#' #tse <- SailaniIPOPData()
+SailaniIPOPData <- function() {
+    mae <- .create_mae("sailani-ipop",
+                       types = list(gutMicrobiota = list("SE" = c("relabundance")),
+                                    nasalMicrobiota = list("SE" = c("relabundance")),
+                                    hostTranscriptome = list("SE" = c("counts")),
+                                    hostProteome = list("SE" = c("counts")),
+                                    hostMetabolome = list("SE" = c("counts")),
+                                    hostCytokines = list("SE" = c("counts")),
+                                    hostClinical = list("SE" = c("counts"))),
+                       coldata = FALSE,
+                       samplemap = FALSE,
+                       has.rowdata = list(gutMicrobiota = FALSE,
+                                          nasalMicrobiota = FALSE,
+                                          hostTranscriptome = FALSE,
+                                          hostProteome = FALSE,
+                                          hostMetabolome = FALSE,
+                                          hostCytokines = FALSE,
+                                          hostClinical = FALSE),
+                       has.coldata = list(gutMicrobiota = TRUE,
+                                          nasalMicrobiota = TRUE,
+                                          hostTranscriptome = TRUE,
+                                          hostProteome = TRUE,
+                                          hostMetabolome = TRUE,
+                                          hostCytokines = TRUE,
+                                          hostClinical = TRUE))
+    mae
+}
+
+
+# #' @rdname SailaniIPOPData
+# #' @importFrom SummarizedExperiment colData<-
+# #' @export
+
+#SailaniIPOPData <- function() {
+#    dataset <- "sailani-ipop"
+#    hub <- ExperimentHub()
+#    tse <- .create_tse(dataset,
+#                       hub = hub,
+#                       assays = c("counts"),
+#                       has.rowdata = TRUE,
+#                       has.coldata = FALSE,
+#                       prefix = "gutMicrobiota")
+#    args <- .get_col_row_map_data(dataset,
+#                                  hub = hub,
+#                                  has.rowdata = FALSE,
+#                                  has.coldata = TRUE)
+#    colData(tse) <- args$colData
+#    tse
+#}
+
+#' @rdname SailaniIPOPData
+#' @export
+sailaniipop <- SailaniIPOPData
---
+++ inst/scripts/make-silverman-ag-metadata.R
@@ -0,0 +1,64 @@
+
+# Base data for all data sets --------------------------------------------------
+library(S4Vectors)
+
+df_Base <- DataFrame(
+  BiocVersion = "3.13",
+  SourceVersion = NA,
+  Coordinate_1_based = TRUE,
+  Species = "Homo sapiens",
+  TaxonomyId = "9606",
+  SourceVersion = Sys.time(),
+  Genome = NA,
+  SourceUrl = "https://doi.org/10.1186/s40168-018-0584-3",
+  DataProvider = NA,
+  Maintainer = "Sudarshan Shetty <sudarshanshetty9@gmail.com>"
+)
+
+df <- rbind(
+  cbind(df_Base,
+        DataFrame(Title = "Silverman Artificial Gut microbiome counts",
+                  Description = paste0("Count matrix for the Silverman microbiome dataset"),
+                  SourceType = "CSV",
+                  RDataClass = "matrix",
+                  DispatchClass = "Rds",
+                  RDataPath = "microbiomeDataSets/silverman-ag/counts.rds",
+                  Tags = NA)),
+  cbind(df_Base,
+        DataFrame(Title = "Silverman Artificial Gut row data",
+                  Description = paste0("Row data for the Silverman microbiome dataset"),
+                  SourceType = "CSV",
+                  RDataClass = "DFrame",
+                  DispatchClass = "Rds",
+                  RDataPath = "microbiomeDataSets/silverman-ag/rowdata.rds",
+                  Tags = NA)),
+  cbind(df_Base,
+        DataFrame(Title = "Silverman Artificial Gut sample data",
+                  Description = paste0("Sample data for the Silverman dataset"),
+                  SourceType = "CSV",
+                  RDataClass = "DFrame",
+                  DispatchClass = "Rds",
+                  RDataPath = "microbiomeDataSets/silverman-ag/coldata.rds",
+                  Tags = NA)),
+  cbind(df_Base,
+        DataFrame(Title = "Silverman Artificial Gut tree data",
+                  Description = paste0("Feature tree data for the Silverman dataset"),
+                  SourceType = "TXT",
+                  RDataClass = NA,
+                  DispatchClass = "FilePath",
+                  RDataPath = "microbiomeDataSets/silverman-ag/rowtree.tre.gz",
+                  Tags = NA)),
+  cbind(df_Base,
+        DataFrame(Title = "Silverman Artificial Gut tree data",
+                  Description = paste0("Reference sequences data for the Silverman dataset"),
+                  SourceType = "TXT",
+                  RDataClass = NA,
+                  DispatchClass = "FilePath",
+                  RDataPath = "microbiomeDataSets/silverman-ag/refseq.fasta.gz",
+                  Tags = NA))
+)
+
+df$Tags <- paste(df$Tags[!is.na(df$Tags)],"Microbiome",collapse = ":",sep="")
+
+write.csv(df, file = "inst/extdata/metadata-silverman-ag.csv", row.names = FALSE)
+
