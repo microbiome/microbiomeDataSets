a288774f17a37d8c55af23c3bf0714de5e00003d
Sudarshan <sudarshanshetty9@gmail.com>
2021-01-14 15:27:43 +0100

    Sprockett th (#11)
    * Sprockett data
    
    Sprockett, D.D., Martin, M., Costello, E.K. et al. Microbiota assembly, structure, and dynamics among Tsimane horticulturalists of the Bolivian Amazon. Nat Commun 11, 3772 (2020). https://doi.org/10.1038/s41467-020-17541-6

---
+++ R/SprockettTHData.R
@@ -0,0 +1,62 @@
+#' Obtain the Sprockett Tsimane Horticulturalists data
+#'
+#' Obtain the microbiome data from Sprockett et al. (2020).
+#'
+#' @details
+#' The SprockettTHData dataset contains 16S rRNA gene based high-throughput
+#' profiling of 1966 Feces, 120 Saliva samples from 319 participants from 
+#' Bolivia, Finland and Bangladesh. These include samples from adults, 
+#' children, and infants. Several participants have longitudinal samples. 
+#' The data consists of 2319 taxa from 2086 samples. The data
+#' set can be used to investigate assembly, structure, and dynamics as well 
+#' as associations between several host related parameters with microbiota.
+#'
+#' Column metadata includes the sex, age, feeding status, delivery mode, 
+#' country, and other information.
+#'
+#' The row metadata of the microbiome data contains taxonomic information 
+#' on the Kingdom, Phylum, Class, Order, Family and Genus, Species and 
+#' lowest taxonomic rank.  
+#' 
+#' The row tree consists of a phylogenetic tree build using sequence 
+#' information of 2319 taxa.  
+#' 
+#' As reference sequences the ASV are provided.
+#' 
+#' All data are downloaded from ExperimentHub and cached for local re-use.
+#'
+#' @return a \linkS4class{TreeSummarizedExperiment}
+#'
+#' @author Sudarshan A. Shetty and Felix G.M. Ernst
+#'
+#' @references
+#' Sprockett, D.D., Martin, M., Costello, E.K. et al. (2020) Microbiota assembly, 
+#' structure, and dynamics among Tsimane horticulturalists of the Bolivian 
+#' Amazon. 
+#' \emph{Nat Commun} 11, 3772 \url{https://doi.org/10.1038/s41467-020-17541-6}
+#'
+#' Subramanian, S., Huq, S., Yatsunenko, T., et al. (2014) Persistent gut microbiota 
+#' immaturity in malnourished Bangladeshi children. 
+#' \emph{Nature} 510, 417-421 \url{https://doi.org/10.1038/nature13421}
+#' 
+#' Vatanen, T., Kostic A.D., d'Hennezel E., et al. (2016) Variation in microbiome LPS 
+#' immunogenicity contributes to autoimmunity in humans. 
+#' \emph{Cell} 165, 842-853 \url{https://doi.org/10.1016/j.cell.2016.04.007}
+#' 
+#' @name SprockettTHData
+#' @export
+#'
+#' @examples
+#' tse <- SprockettTHData()
+SprockettTHData <- function() {
+  dataset <- "sprockett-th"
+  tse <- .create_tse(dataset,
+                     assays = c("counts"),
+                     has.rowdata = TRUE,
+                     has.coldata = TRUE,
+                     has.rowtree = TRUE,
+                     has.refseq = TRUE)
+  tse
+}
+
+
---
+++ inst/scripts/make-sprockett-th-metadata.R
@@ -0,0 +1,64 @@
+
+# Base data for all data sets --------------------------------------------------
+library(S4Vectors)
+
+df_Base <- DataFrame(
+  BiocVersion = "3.13",
+  SourceVersion = NA,
+  Coordinate_1_based = TRUE,
+  Species = "Homo sapiens",
+  TaxonomyId = "9606",
+  SourceVersion = Sys.time(),
+  Genome = NA,
+  SourceUrl = "https://www.nature.com/articles/s41467-020-17541-6#Abs1",
+  DataProvider = NA,
+  Maintainer = "Sudarshan Shetty <sudarshanshetty9@gmail.com>"
+)
+
+df <- rbind(
+  cbind(df_Base,
+        DataFrame(Title = "Sprockett Tsimane Horticulturalists microbiome counts",
+                  Description = paste0("Count matrix for the Sprockett microbiome dataset"),
+                  SourceType = "CSV",
+                  RDataClass = "matrix",
+                  DispatchClass = "Rds",
+                  RDataPath = "microbiomeDataSets/sprockett-th/counts.rds",
+                  Tags = NA)),
+  cbind(df_Base,
+        DataFrame(Title = "Sprockett Tsimane Horticulturalists row data",
+                  Description = paste0("Row data for the Sprockett microbiome dataset"),
+                  SourceType = "CSV",
+                  RDataClass = "DFrame",
+                  DispatchClass = "Rds",
+                  RDataPath = "microbiomeDataSets/sprockett-th/rowdata.rds",
+                  Tags = NA)),
+  cbind(df_Base,
+        DataFrame(Title = "Sprockett Tsimane Horticulturalists sample data",
+                  Description = paste0("Sample data for the Sprockett dataset"),
+                  SourceType = "CSV",
+                  RDataClass = "DFrame",
+                  DispatchClass = "Rds",
+                  RDataPath = "microbiomeDataSets/sprockett-th/coldata.rds",
+                  Tags = NA)),
+  cbind(df_Base,
+        DataFrame(Title = "Sprockett Tsimane Horticulturalists tree data",
+                  Description = paste0("Feature tree data for the Sprockett dataset"),
+                  SourceType = "TXT",
+                  RDataClass = NA,
+                  DispatchClass = "FilePath",
+                  RDataPath = "microbiomeDataSets/sprockett-th/rowtree.tre.gz",
+                  Tags = NA)),
+  cbind(df_Base,
+        DataFrame(Title = "Sprockett Tsimane Horticulturalists reference sequence data",
+                  Description = paste0("Reference sequences data for the Sprockett dataset"),
+                  SourceType = "TXT",
+                  RDataClass = NA,
+                  DispatchClass = "FilePath",
+                  RDataPath = "microbiomeDataSets/sprockett-th/refseq.fasta.gz",
+                  Tags = NA))
+)
+
+df$Tags <- paste(df$Tags[!is.na(df$Tags)],"Microbiome",collapse = ":",sep="")
+
+write.csv(df, file = "inst/extdata/metadata-sprockett-th.csv", row.names = FALSE)
+
