14d6b1706c23d9d68c732bd1a499e411a687e74f
antagomir <leo.lahti@iki.fi>
2021-04-12 20:43:36 +0300

    HintikkaXOData set initialization

---
+++ inst/scripts/make-hintikka-bc-data.R
@@ -0,0 +1,67 @@
+library(readxl)
+library(tidyverse)
+
+# Read metabolite data
+nmr <- read_excel("NMR_Quantification.xlsx") %>%
+         rename(Rat = "RAT ID") %>%
+         rename(Group = "Group ID") %>%
+	 select(-Group)
+
+# Sample metadata
+meta <- read_excel("Otut_abundanssit_metadata_ok.xlsx", sheet = "Metadata") %>%
+         rename(Rat = "Rotta ID") %>%
+         rename(Group = "Ryhmä nro") %>%
+         rename(Batch = "Sekvensointi ID") %>%
+	 select(-Batch) %>% # Same as "Rat"
+	 select(-Group) %>% # Not needed
+         rename(Site = "Suolen osa") %>%
+	 rename(Sample = Rat) %>% # These are sample IDs
+         mutate(Rat = str_remove(Sample, "^[P|C]")) %>%	 # These are subject IDs 
+	 mutate(Fat = factor(str_trim(str_remove(str_remove(Diet, "\\+ XOS"), "-fat")), levels = c("Low", "High"))) %>%
+	 mutate(XOS = as.numeric(str_replace(replace_na(str_match(meta$Diet, "XOS"), 0), "XOS", "1")))
+	 
+# Read microbiota data
+ngs <- read_excel("Otut_abundanssit_metadata_ok.xlsx", sheet = "OTU table siisti")
+## Separate taxonomy table and abundances
+tax <- ngs[, 1:7] %>%
+         rename(OTU = "OTU ID") %>%
+         rename(Phylum = "D1") %>%
+         rename(Class = "D2") %>%
+         rename(Order = "D3") %>%
+         rename(Family = "D4") %>%
+         rename(Genus = "D5") %>%
+         rename(Species = "D6") 	 
+
+otu <- ngs[, 8:ncol(ngs)]
+
+
+# Only Cecum data was used in the paper; separate metadata and blood+other measurements
+inds <- which(meta$Site == "Cecum") # Check matching.txt
+vars <- c("Sample", "Rat", "Site", "Diet", "Fat", "XOS")
+meta_cecum <- meta[inds, vars]
+
+# Manually checked that the sample order corresponds;
+# let us rename the samples so they have same names in the different tables
+otu_cecum <- otu[, inds]
+colnames(otu_cecum) <- meta_cecum$Sample
+
+# Biomarkers
+# Manually checked that the sample order corresponds;
+# let us rename the samples so they have same names in the different tables
+bm <- t(meta[inds, -match(vars, colnames(meta))])
+colnames(bm) <- meta_cecum$Sample
+
+
+# Convert to TSE object
+nmr$Rat <- NULL # Can be removed after matching
+nmr <- t(nmr) # features x samples
+
+
+saveRDS(otu_cecum, file = "microbiome_counts.rds")
+saveRDS(tax, file = "microbiome_rowdata.rds")
+saveRDS(meta_cecum, file = "coldata.rds")
+saveRDS(nmr, file = "metabolites.rds")
+saveRDS(bm, file = "biomarkers.rds")
+
+
+
--- inst/scripts/make-hintikka-bc-metadata.R
+++ inst/scripts/make-hintikka-bc-metadata.R
@@ -1,6 +1,8 @@
 # Base data for all data sets --------------------------------------------------
 library(S4Vectors)
 
+
+
 BiocVersion <- "3.13"
 path <- paste0("microbiomeDataSets/",BiocVersion,"/")
 
@@ -8,12 +10,12 @@ df_Base <- DataFrame(
     BiocVersion = BiocVersion,
     SourceVersion = NA,
     Coordinate_1_based = TRUE,
-    Species = "Mus musculus",
-    TaxonomyId = "10090",
+    Species = "Rattus norvegicus",
+    TaxonomyId = "10116",
     SourceVersion = Sys.time(),
     Genome = NA,
-    SourceUrl = "https://doi.org/10.3390/ijerph18084049",
-    DataProvider = NA,
+    SourceUrl = "https://ndownloader.figshare.com/files/",
+    DataProvider = "University of Jyvaskyla",
     Maintainer = "Leo Lahti <leo.lahti@iki.fi>"
 )
 
@@ -22,7 +24,7 @@ df <- rbind(
     cbind(df_Base,
           DataFrame(Title = "Hintikka XO microbiome counts",
                     Description = paste0("Count matrix for the Hintikka microbiome dataset"),
-                    SourceType = "CSV",
+                    SourceType = "XLSX",
                     RDataClass = "matrix",
                     DispatchClass = "Rds",
                     RDataPath = paste0(path,"hintikka-xo/microbiome_counts.rds"),
@@ -30,7 +32,7 @@ df <- rbind(
     cbind(df_Base,
           DataFrame(Title = "Hintikka XO microbiome row data",
                     Description = paste0("Row data for the Hintikka microbiome dataset"),
-                    SourceType = "CSV",
+                    SourceType = "XLSX",
                     RDataClass = "DFrame",
                     DispatchClass = "Rds",
                     RDataPath = paste0(path,"hintikka-xo/microbiome_rowdata.rds"),
@@ -38,15 +40,15 @@ df <- rbind(
     cbind(df_Base,
           DataFrame(Title = "Hintikka XO metabolites counts",
                     Description = paste0("Count matrix for the Hintikka metabolites dataset"),
-                    SourceType = "CSV",
+                    SourceType = "XLSX",
                     RDataClass = "matrix",
                     DispatchClass = "Rds",
-                    RDataPath = paste0(path,"hintikka-xo/metabolite_counts.rds"),
+                    RDataPath = paste0(path,"hintikka-xo/metabolites.rds"),
                     Tags = NA)),
     cbind(df_Base,
           DataFrame(Title = "Hintikka XO sample data",
                     Description = paste0("Sample data for the Hintikka dataset"),
-                    SourceType = "CSV",
+                    SourceType = "XLSX",
                     RDataClass = "DFrame",
                     DispatchClass = "Rds",
                     RDataPath = paste0(path,"hintikka-xo/coldata.rds"),
