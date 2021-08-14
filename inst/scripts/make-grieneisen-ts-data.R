9f2790e623b397bf6575e44c16ea0a15b1e530f3
YagmurSimsekk <55834139+YagmurSimsekk@users.noreply.github.com>
2021-08-14 22:17:37 +0300

    Update make-grieneisen-ts-data.R

--- inst/scripts/make-grieneisen-ts-data.R
+++ inst/scripts/make-grieneisen-ts-data.R
@@ -2,11 +2,13 @@ library(SummarizedExperiment)
 library(TreeSummarizedExperiment)
 library(dplyr)
 library(phyloseq)
+library(ape)
 
 #Sample phenotype data
 samples <- read.csv("git_metadata_and_community_phenotypes.csv")
 rownames(samples) <- samples$sample
-samples$sample <- NULL
+as.factor(c("baboon_id", "sex", "social_group", "season" ,"plate"))
+samples$collection_date <- as.Date(samples$collection_date, format="%Y-%m-%d")
 
 #Abundance table
 counts <- readRDS("git_ASV_table.RDS")
@@ -17,8 +19,8 @@ ref_seq <- data.frame(ASV_Sequence)
 rownames(ref_seq) <- counts$asv_id
 
 #Taxonomic mapping table
-tax_1 <- select(counts, domain, phylum, class, order, family, genus, asv_id)
-rownames(tax_1) <- counts$asv_id
+tax <- select(counts, domain, phylum, class, order, family, genus, asv_id)
+rownames(tax) <- counts$asv_id
 
 # In order to make sure that the samples match between the abundance and phenodata tables
 counts_trimmed <- counts[, rownames(samples)]
@@ -26,41 +28,10 @@ counts_trimmed <- counts[, rownames(samples)]
 #Phylogenetic tree
 pseq <- readRDS("asv_10prev_phyloseq_for_lahti.RDS")
 tree <- phy_tree(pseq)
-
-#converting csv file into rds file
-saveRDS(samples, "samples.rds")
-
-#Metadata Table
-samples_1 <- samples %>% dplyr::rename(c(
-  Number = "X",
-  Baboon = "baboon_id",
-  Date = "collection_date",
-  Sex = "sex",
-  Age = "age",
-  Group = "social_group" ,
-  "Group_Size" = "group_size",
-  "Rain_Month_mm" = "rain_month_mm",
-  Season = "season",
-  Month = "month",
-  Readcount = "readcount",
-  Plate = "plate",
-  "Hydro_Year" = "hydro_year",
-  "Post_PCR_DNA_ng" = "post_pcr_dna_ng",
-  "Diet_Shannon_h" = "diet_shannon_h",
-  "ASV_Richness" = "asv_richness",
-  "ASV_Shannon_h" = "asv_shannon_h",
-  PC1_bc = "pc1_bc",
-  PC2_bc = "pc2_bc",
-  PC3_bc = "pc3_bc",
-  PC4_bc = "pc4_bc",
-  PC5_bc = "pc5_bc",
-  ))
-
-samples <- data.frame(samples_1)
+tree <- makeNodeLabel(tree, method="number", prefix='n')
 
 #Taxonomy Table
-saveRDS(tax_1, "tax.rds")
-tax <- tax_1  %>% dplyr::rename(c(
+tax <- tax  %>% dplyr::rename(c(
   Domain = "domain",
   Phylum = "phylum",
   Class = "class",
@@ -73,12 +44,13 @@ tax <- tax_1  %>% dplyr::rename(c(
 #Arranging counts
 rownames(counts) <- counts$asv_id
 counts[, c("asv_id", "asv_sequence", "domain", "phylum", "class", "order", "family", "genus")]  <- NULL
+counts <- as.matrix(counts)
 
 #sequence information
 refSeq <- DNAStringSet(ASV_Sequence, start=NA, end=NA, width=NA, use.names=TRUE)
 
 tse <- TreeSummarizedExperiment(assays = list(counts = counts_trimmed),
-                                colData = samples_1,
+                                colData = samples,
                                 rowData = tax,
                                 referenceSeq = refSeq)
 
@@ -86,7 +58,9 @@ tse <- TreeSummarizedExperiment(assays = list(counts = counts_trimmed),
 tse <- tse[tree$tip.label, ]
 
 rowTree(tse) <- tree
-tse 
+
+# Collapse the tree
+tree <- ape::keep.tip(phy = rowTree(tse), tip = rowLinks(tse)$nodeNum)
 
 rowdata <- rowData(tse)
 coldata <- colData(tse)
@@ -94,9 +68,7 @@ counts <- assay(tse, "counts")
 rowtree <- rowTree(tse)
 refseq <- referenceSeq(tse)
 
-unlink(c("samples.rds","tax.rds"))
-
-data <- "/MicrobiomeDataSets/3.14/"
+data <- "/microbiomeDataSets/3.14/"
 
 saveRDS(counts, file = paste0(data,"counts.rds"))
 saveRDS(coldata, file = paste0(data,"coldata.rds"))
