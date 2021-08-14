14b82412a1e923b7907910c3f772e84b3084545b
antagomir <leo.lahti@iki.fi>
2021-08-14 12:32:07 +0300

    hide the tests until EHub is updated

--- R/HintikkaXOData.R
+++ R/HintikkaXOData.R
@@ -46,6 +46,7 @@
 #'
 #' @examples
 #' # Retrieve the MAE data
+#' \dontrun{
 #' mae <- HintikkaXOData()
 #'
 #' # List the experiments in this MultiAssayExperiment object
@@ -65,7 +66,7 @@
 #'
 #' # microbiota rowData
 #' taxtab <- rowData(mae[["microbiota"]])
-#' 
+#' } 
 HintikkaXOData <- function() {
 
     mae <- .create_mae("3.14/hintikka-xo",
